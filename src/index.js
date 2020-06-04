db05be329af1f1bb05221c91853f9ff5ddb74668
illia <illia.solonar@gmail.com>
2020-06-04 19:39:45 +0300

    Fixes

--- src/index.js
+++ src/index.js
@@ -18,14 +18,14 @@ const filepaths = {
 export class Game {
 
     async start() {
-        diContainer.configure('webRtcNetworkClient', {serverIp: '127.0.0.1', signalingServerPort: '8080'});
+        /*diContainer.configure('webRtcNetworkClient', {serverIp: '127.0.0.1', signalingServerPort: '8080'});
         // eslint-disable-next-line no-undef
         diContainer.configure('messageEncoderDecoder', {protoBundle: require('../../common/proto/bundle.json')});
 
         const webRtcNetworkClient = await diContainer.get('webRtcNetworkClient');
-        await webRtcNetworkClient.connect();
+        await webRtcNetworkClient.connect();*/
 
-        /*this.frontendFacade = await createFrontendFacade(filepaths);
+        this.frontendFacade = await createFrontendFacade(filepaths);
 
         const sprite = await this.frontendFacade.createSprite('aim');
         sprite.center.set(0.5, 0.5);
@@ -37,27 +37,27 @@ export class Game {
         const gameObject = await this.frontendFacade.createObject("player1", FlyingObject, "smallSpaceFighter");
         // TODO rename switchControls to associate controls?
         this.frontendFacade.switchControls(controls.FLYING_OBJECT_CONTROLS, gameObject);
-        this.frontendFacade.startGameLoop();*/
+        this.frontendFacade.startGameLoop();
     }
 
     async addSpaceships() {
         const spaceShip1 = await this.frontendFacade.createObject("bot1", FlyingObject, "smallSpaceFighter");
-        spaceShip1.object3d.position.z = -4;
-        spaceShip1.object3d.position.x = 5;
-        spaceShip1.object3d.position.y = 5;
-        spaceShip1.speedAbs = 0;
+        spaceShip1.position.z = -4;
+        spaceShip1.position.x = 5;
+        spaceShip1.position.y = 5;
+        spaceShip1.velocity.z = 0;
 
         const spaceShip2 = await this.frontendFacade.createObject("bot2", FlyingObject, "smallSpaceFighter");
-        spaceShip2.object3d.position.z = -10;
-        spaceShip2.object3d.position.x = -3.5;
-        spaceShip2.object3d.position.y = 4.5;
-        spaceShip2.speedAbs = 0;
+        spaceShip2.position.z = -10;
+        spaceShip2.position.x = -3.5;
+        spaceShip2.position.y = 4.5;
+        spaceShip2.velocity.z = 0;
 
         const spaceShip3 = await this.frontendFacade.createObject("bot3", FlyingObject, "smallSpaceFighter");
-        spaceShip3.object3d.position.z = -100;
-        spaceShip3.object3d.position.x = -3.5;
-        spaceShip3.object3d.position.y = 4.5;
-        spaceShip3.speedAbs = 0;
+        spaceShip3.position.z = -100;
+        spaceShip3.position.x = -3.5;
+        spaceShip3.position.y = 4.5;
+        spaceShip3.velocity.z = 0;
     }
 
 }
