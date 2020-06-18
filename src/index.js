b465a00e0dfe9bd232317e28781194ee11584f4d
illia <illia.solonar@gmail.com>
2020-06-18 18:28:38 +0300

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/**
+ * @typedef {import('./engine/net/service/MultiplayerService').default} MultiplayerService
+ */
+
 // initialize required modules
 import './engine/frontend';
 import './engine/net';
@@ -6,6 +10,8 @@ import Engine from './engine';
 import {controls} from "./engine/object-control";
 import {cameraManagers} from "./engine/frontend/camera";
 import FlyingObject from "./engine/physics/object/FlyingObject";
+import Emitter from './engine/util/Emitter';
+//import EventTarget from 'events';
 
 const filepaths = {
     "models": {
@@ -16,7 +22,7 @@ const filepaths = {
     }
 };
 
-export class Game extends EventTarget {
+export class Game extends Emitter {
 
     constructor() {
         super();
@@ -26,18 +32,22 @@ export class Game extends EventTarget {
     async start() {
         this._configure();
 
-        const webRtcNetworkClient = await this.diContainer.get('webRtcNetworkClient');
-        await webRtcNetworkClient.connect();
+        //const webRtcNetworkClient = await this.diContainer.get('webRtcNetworkClient');
+        //await webRtcNetworkClient.connect();
+        /** @type {MultiplayerService} */
+        const multiplayerService = await this.diContainer.get('multiplayerService');
+        await multiplayerService.connect();
+        await multiplayerService.requestSpawn();
 
         this.frontendFacade = await Engine.createFrontendFacade(filepaths);
         await this._addAimSprite();
 
-        //await this.addSpaceships();
-        //const gameObject = await this.frontendFacade.createObject("player1", FlyingObject, "smallSpaceFighter");
+        // await this.addSpaceships();
+        // const gameObject = await this.frontendFacade.createObject("player1", FlyingObject, "smallSpaceFighter");
         // TODO rename switchControls to associate controls?
-        //await this.frontendFacade.attachControls(controls.FLYING_OBJECT_REMOTE_CONTROLS_TEST, gameObject);
-        //await this.frontendFacade.attachCameraManager(cameraManagers.FLYING_OBJECT_CAMERA_MANAGER);
-        this.frontendFacade.startGameLoop();
+        // await this.frontendFacade.attachControls(controls.FLYING_OBJECT_REMOTE_CONTROLS_TEST, gameObject);
+        // await this.frontendFacade.attachCameraManager(cameraManagers.FLYING_OBJECT_CAMERA_MANAGER);
+        // this.frontendFacade.startGameLoop();
     }
 
     _configure() {
