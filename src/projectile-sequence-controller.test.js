d151a7759efb0291785f0482effd2483d3857c81
illia <illia.solonar@gmail.com>
2020-09-22 18:21:14 +0300

    Test implementations

--- src/projectile-sequence-controller.test.js
+++ src/projectile-sequence-controller.test.js
@@ -8,27 +8,37 @@ import DirectionalProjectile from "./engine/physics/object/DirectionalProjectile
 import ProjectileSequenceController from "./engine/object-control/projectile/ProjectileSequenceController";
 import AuthoritativeStateManager from "./engine/state/authoritative-state-manager/AuthoritativeStateManager";
 import {DiContainer} from "di-container-js";
+import FlyingObject from "./engine/physics/object/FlyingObject";
 
-test('should build dependency graph', async () => {
-    const geometry = new THREE.BoxGeometry(10, 10, 10);
+test('should return find intersection if inside of object', async () => {
+    const direction = new THREE.Vector3(1, 0, 0);
+    const projectile1 = createProjectile(new THREE.Vector3(0, 0, 0), direction);
 
-    const material = new THREE.MeshBasicMaterial();//this.createProjectileMaterial();
-    let model = new THREE.Mesh(geometry, material);
+    const projectileSequenceController = createProjectileSequenceController();
+    projectileSequenceController.projectiles = [projectile1];
 
+    const gameObject = createDummyBox();
+
+    const result = projectileSequenceController.findHitsWithObject(gameObject);
+    expect(result.length).toBe(1);
+});
+
+test('should not check intersections with all projectiles if first one is behind an object', async () => {
     const direction = new THREE.Vector3(1, 0, 0);
     const projectile1 = createProjectile(new THREE.Vector3(12, 0, 0), direction);
     const projectile2 = createProjectile(new THREE.Vector3(13, 0, 0), direction);
     const projectile3 = createProjectile(new THREE.Vector3(14, 0, 0), direction);
 
-    const diContainer = new DiContainer();
-    const stateManager = new AuthoritativeStateManager(diContainer, null);
-    const projectileSequenceController = new ProjectileSequenceController(null, stateManager, diContainer);
+    const projectileSequenceController = createProjectileSequenceController();
     projectileSequenceController.projectiles = [projectile1, projectile2, projectile3];
 
-    // const gameObject = ...;
-    // const result = projectileSequenceController.findHitsWithObject(gameObject);
-    // ...
+    const gameObject = createDummyBox();
 
+    projectileSequenceController.isProjectileIntersectsWithObject = jest.fn(projectileSequenceController.isProjectileIntersectsWithObject);
+    const result = projectileSequenceController.findHitsWithObject(gameObject);
+
+    expect(result).toBe(null);
+    expect(projectileSequenceController.isProjectileIntersectsWithObject.mock.calls.length).toBe(1);
 });
 
 function createProjectile(position, direction) {
@@ -39,4 +49,23 @@ function createProjectile(position, direction) {
     directionalProjectile.position.copy(position);
 
     return directionalProjectile;
+}
+
+function createProjectileSequenceController() {
+    const diContainer = new DiContainer();
+    const stateManager = new AuthoritativeStateManager(diContainer, null);
+    return new ProjectileSequenceController(null, stateManager, diContainer);
+}
+
+function createDummyBox() {
+    const geometry = new THREE.BoxGeometry(10, 10, 10);
+
+    const material = new THREE.MeshBasicMaterial();
+    material.side = THREE.DoubleSide;
+    let model = new THREE.Mesh(geometry, material);
+
+    const gameObject = new FlyingObject(null, model);
+    gameObject.position = new THREE.Vector3(0, 0, 0);
+
+    return gameObject;
 }
